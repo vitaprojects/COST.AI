d2c69134c7d5c2ccc58ed8a7bad8338584e45df1
diptomistry <diptomistrybd@gmail.com>
2025-03-15 14:37:28 +0600

    Refactor main.py to remove commented-out FastAPI initialization code

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from routes import calculate_router
-
-# # Initialize FastAPI
-# app = FastAPI()
-
-# # Allow all origins
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allows all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Allows all HTTP methods
-#     allow_headers=["*"],  # Allows all headers
-# )
-
-# # Include routes
-# app.include_router(calculate_router)
-
-# # Root endpoint
-# @app.get("/")
-# def root():
-#     return {"message": "Delivery Cost Calculator API"}
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
